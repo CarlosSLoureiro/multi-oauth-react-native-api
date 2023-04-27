e445c0b9e11d89aff702c8f829ca83e7d79557ee
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-04-26 22:32:28 -0300

    feat: resolve teste falhado propositalmente

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -3,7 +3,7 @@ import type Env from '@env';
 import swaggerComponents from './swagger/components';
 import * as html from './swagger.html';
 
-export function MountSwaggerUI (env: Env, swaggerPaths: Object): string {
+export function MountSwaggerUI (env: Env, swaggerPaths: object): string {
   return html.default
     .replace('SWAGGER_PATHS', JSON.stringify(swaggerPaths))
     .replace('SWAGGER_COMPONENTS', JSON.stringify(swaggerComponents))
