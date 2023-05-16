b14d886e1827c172735b7a8bf264101089790d3b
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-05-16 19:28:30 -0300

    feat: cria base para user service

--- src/containers/services.ts
+++ src/containers/services.ts
@@ -1,9 +1,11 @@
 import { type Container } from 'inversify';
 
 import AuthService from '@services/auth';
+import UserService from '@services/user';
 
 export default abstract class ReigsterServices {
   public static config (container: Container): void {
     container.bind<AuthService>(AuthService).toSelf();
+    container.bind<UserService>(UserService).toSelf();
   }
 }
---
+++ src/services/user.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from 'inversify';
+
+import type User from '@models/user';
+import type UserInterface from '@models/user.interface';
+
+import UserRepository from '@repository/user';
+import UserRepositoryInterface from '@repository/user.interface';
+
+@injectable()
+
+export default class UserService {
+  private readonly userRepository: UserRepositoryInterface;
+
+  constructor (@inject(UserRepository) userRepository?: UserRepositoryInterface) {
+    this.userRepository = userRepository;
+  }
+
+  public async create (user: UserInterface): Promise<User> {
+    return await this.userRepository.create(user);
+  }
+}
