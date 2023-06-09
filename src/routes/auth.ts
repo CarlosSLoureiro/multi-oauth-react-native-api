b2e95920b3e52aa965f5a865bf55723d2b699b84
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-09 02:09:29 -0300

    feat: adiciona rota para verificar o auth token

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,11 @@
 import container from '@container';
 
 import AuthController from '@controllers/auth';
+import AuthenticatedMiddleware from '@middlewares/authenticated';
 
-import swaggerData from '@docs/swagger/auth';
+import swaggerDataAuth from '@docs/swagger/auth/auth';
+import swaggerDataAuthCheck from '@docs/swagger/auth/check';
+import swaggerDataAuthError from '@docs/swagger/auth/error';
 
 import setClientData from '@utils/client-data/set';
 
@@ -13,12 +16,20 @@ import passport from 'passport';
 
 export default abstract class AuthRoutes {
   public static config (router: Router, swaggerPaths: object): void {
+    let route: string;
     const authController = container.get<AuthController>(AuthController);
 
-    swaggerPaths[`/auth`] = swaggerData;
-    router.post(`/auth`, RoutesUtils.getAsync(authController.authenticateWithPassword));
+    route = `/login`;
+    swaggerPaths[route] = swaggerDataAuth;
+    router.post(route, RoutesUtils.getAsync(authController.authenticateWithPassword));
 
-    router.get(`/auth/error`, authController.error);
+    route = `/auth/check`;
+    swaggerPaths[route] = swaggerDataAuthCheck;
+    router.get(route, AuthenticatedMiddleware.handle, RoutesUtils.getAsync(authController.check));
+
+    route = `/auth/error`;
+    swaggerPaths[route] = swaggerDataAuthError;
+    router.get(route, authController.error);
 
     router.get(`/auth/google`, setClientData, passport.authenticate(`google`, { scope: [`profile`, `email`], session: false }));
 
