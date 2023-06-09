3c6276d1cfd60e99cdc4b6f6278f822621129123
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-09 00:56:30 -0300

    feat: permite swagger ui adicionar auth token

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,19 +1,32 @@
 import swaggerComponents from './swagger/components';
 
 export function MountSwaggerConfig (swaggerPaths: object): [object, object] {
-  return [{
-    openapi: `3.0.3`,
-    info: {
-      title: process.env.API_NAME,
-      version: process.env.API_VERSION
-    },
-    paths: swaggerPaths,
-    components: swaggerComponents
-  },
-  {
-    swaggerOptions: {
-      defaultModelsExpandDepth: -1
+  const securityScheme = {
+    bearerAuth: {
+      type: `http`,
+      scheme: `bearer`,
+      bearerFormat: `JWT`
     }
-  }
+  };
+
+  return [
+    {
+      openapi: `3.0.3`,
+      info: {
+        title: process.env.API_NAME,
+        version: process.env.API_VERSION
+      },
+      paths: swaggerPaths,
+      components: {
+        ...swaggerComponents,
+        securitySchemes: securityScheme
+      },
+      security: [{ bearerAuth: [] }]
+    },
+    {
+      swaggerOptions: {
+        defaultModelsExpandDepth: -1
+      }
+    }
   ];
 }
