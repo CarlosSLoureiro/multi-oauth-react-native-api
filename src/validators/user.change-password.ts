4fd0f5af5a3cee0a69ac5670c057b49a063e0eed
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-18 19:23:07 -0300

    fix: resolve validation para alterar senha

--- src/validators/user.change-password.ts
+++ src/validators/user.change-password.ts
@@ -8,14 +8,14 @@ import Joi from "joi";
 export default abstract class UserChangePasswordValidator {
   public static validate (): ValidateInterface {
     const schema = Joi.object<UserChangePasswordRequest>({
-      currentPassword: Joi.string().label(`Current Password`),
+      currentPassword: Joi.string().label(`Current Password`).allow(``),
       newPassword: Joi.string().label(`New Password`)
         .min(8)
         .regex(/^(?=.*[A-Za-z])(?=.*\d)/)
-        .message(`The "Password" must contain at least one letter and one number`)
+        .message(`The "New Password" must contain at least one letter and one number`)
         .required(),
       confirmPassword: Joi.string().label(`Confirm New Password`).required()
-        .valid(Joi.ref(`password`)).messages({
+        .valid(Joi.ref(`newPassword`)).messages({
           'any.only': `The "New Password" and "Confirm New Password" fields must match`
         })
     }).messages(messages);
