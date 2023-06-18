6a9fa298a5c03574537a61777d8b9728203cb0cc
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-18 18:03:45 -0300

    refactor: verifica dados do auth token no banco de dados

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -1,3 +1,9 @@
+import container from '@container';
+
+import { injectable } from 'inversify';
+
+import AuthService from '@services/auth';
+
 import GenericError from '@errors/generic.error';
 
 import { type AuthenticatedUser } from './authenticated.types';
@@ -6,23 +12,32 @@ import { type NextFunction, type Request, type Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import jwt, { TokenExpiredError } from 'jsonwebtoken';
 
+@injectable()
+
 export default class AuthenticatedMiddleware {
-  static handle (req: Request, res: Response, next: NextFunction): void {
-    const token = req.headers.authorization?.split(` `)[1];
+  public handle (request: Request, response: Response, next: NextFunction): void {
+    const authService = container.get<AuthService>(AuthService);
+    const token = request.headers.authorization?.split(` `)[1];
 
     if (!token) {
       throw new GenericError(`Must be authenticated to access`, StatusCodes.UNAUTHORIZED);
     }
 
     try {
-      const decoded = jwt.verify(token, process.env.API_SECRET) as AuthenticatedUser;
+      const authenticatedUser = jwt.verify(token, process.env.API_SECRET) as AuthenticatedUser;
 
-      req.user = decoded;
-
-      next();
+      authService.verifyUserByIdAndPassword(authenticatedUser.id, authenticatedUser.password).then(user => {
+        request.user = user;
+        next();
+      }).catch(e => {
+        console.log(`check error > `, e);
+        throw e;
+      });
     } catch (err) {
       if (err instanceof TokenExpiredError) {
         throw new GenericError(`Expired auth token`, StatusCodes.UNAUTHORIZED);
+      } else if (err instanceof GenericError) {
+        throw new GenericError(err.message, StatusCodes.UNAUTHORIZED);
       } else {
         throw new GenericError(`Invalid auth token`, StatusCodes.UNAUTHORIZED);
       }
