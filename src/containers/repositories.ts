5c5c78b8a03606612755f610b6a6511f46b8bc23
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-19 23:40:32 -0300

    feat: registra repository de logins no container do inversify

--- src/containers/repositories.ts
+++ src/containers/repositories.ts
@@ -1,9 +1,11 @@
 import { type Container } from 'inversify';
 
+import LoginsRepository from '@repository/logins';
 import UserRepository from '@repository/user';
 
 export default abstract class RegisterRepositories {
   public static config (container: Container): void {
+    container.bind<LoginsRepository>(LoginsRepository).toSelf();
     container.bind<UserRepository>(UserRepository).toSelf();
   }
 }
