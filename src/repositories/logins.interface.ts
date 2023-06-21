d31df124a8d4fceed16e6ba28bae119aed96e3e5
Carlos Loureiro <carlos.loureiro@pebmed.com.br>
2023-06-21 00:58:59 -0300

    feat: cria método para buscar listagem de logins no repository

--- src/repositories/logins.interface.ts
+++ src/repositories/logins.interface.ts
@@ -3,4 +3,5 @@ import type LoginsInterface from '@models/logins.interface';
 
 export default interface LoginsRepositoryInterface {
   create: (loginData: Omit<LoginsInterface, "id">) => Promise<Logins>;
+  findManyByPage: (list: number) => Promise<Array<Logins>>;
 }
