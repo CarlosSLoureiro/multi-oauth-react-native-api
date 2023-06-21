b3fd6f38cac62e9c7cbc16b66938bc2dbe988045
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-21 20:49:18 -0300

    refactor: usa lib correta para google oauth2

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -1,10 +1,12 @@
+import { type OAuth2Profile } from '@auth';
+
 import { type Request } from 'express';
-import passport, { type Profile } from 'passport';
-import { OAuth2Strategy, type VerifyFunction } from 'passport-google-oauth';
+import passport from 'passport';
+import { Strategy, type VerifyCallback } from 'passport-google-oauth2';
 
 export default abstract class GoogleAuth {
-  private static readonly verify = (req: Request, accessToken: string, refreshToken: string, profile: Profile, done: VerifyFunction): void => {
-    req.body = profile;
+  private static readonly verify = (request: Request, accessToken: string, refreshToken: string, profile: OAuth2Profile, done: VerifyCallback): void => {
+    request.body = profile;
     done(null, profile);
   };
 
@@ -12,7 +14,7 @@ export default abstract class GoogleAuth {
     const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 
     passport.use(
-      new OAuth2Strategy(
+      new Strategy(
         {
           clientID: GOOGLE_CLIENT_ID,
           clientSecret: GOOGLE_CLIENT_SECRET,
