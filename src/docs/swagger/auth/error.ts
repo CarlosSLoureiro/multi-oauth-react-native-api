6528f811d924cb11527acf73b8c360eed351d914
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-06-25 17:40:49 -0300

    feat: renomeia descrição da rota no swagger

--- src/docs/swagger/auth/error.ts
+++ src/docs/swagger/auth/error.ts
@@ -2,12 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 
 export default {
   get: {
-    summary: `OAuth2 error route.`,
+    summary: `OAuth error route.`,
     tags: [`Authentication`],
     security: [],
     responses: {
       [StatusCodes.TEMPORARY_REDIRECT]: {
-        description: `In case of some OAuth2 way throws error, it will route to this route that will redirect the user to client with encrypted data query as error.`,
+        description: `In case of some OAuth way throws error, it will route to this route that will redirect the user to client with encrypted data query as error.`,
         headers: {
           Location: {
             description: `https://web-version-app.carlosloureiro.xyz/?data=ENCRYPTED_DATA | exp://DEBUGGER_HOST/--/?data=ENCRYPTED_DATA`
